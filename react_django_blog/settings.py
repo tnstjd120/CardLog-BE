f4ecb306fe3ba725c06fe30640a1e2409b86fe9e
tnstjd120 <tnstjd120@gmail.com>
2023-03-07 00:13:10 +0900

    feat: DB sqlite3 -> mysql로 변경

--- react_django_blog/settings.py
+++ react_django_blog/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'react_django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CardLogDB',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
