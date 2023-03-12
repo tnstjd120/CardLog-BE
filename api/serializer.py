9bfb4017f23104f8a0df635d914cd7369b6a94af
tnstjd120 <tnstjd120@gmail.com>
2023-03-12 18:34:16 +0900

    feat: user, link, category serializers 작업

--- api/serializer.py
+++ api/serializer.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, User, LinkList, Category
 
+# ========== User Serializers ==========
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'email', 'profile_img', 'about', 'blog_name')
+
+class LinkListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LinkList
+        fields = ('id', 'icon_type', 'url')
+
+
+# ========== Category Serializers ==========
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'create_at')
+
+
+# ========== Post Serializers ==========
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'title', 'content', 'update_at', 'create_at')
+        fields = ('id', 'post_type', 'title', 'content', 'bg_color', 'text_color', 'thumbnail', 'update_at', 'create_at')
