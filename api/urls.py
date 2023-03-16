e268f70640f5d9f9fa53b822cfaf67c5b9c5b59e
tnstjd120 <tnstjd120@gmail.com>
2023-03-16 17:21:17 +0900

    feat: urlpatterns 변경

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import PostCardList, PostDetail
 
 urlpatterns = [
-    path('cards', PostCardList.as_view()),
-    path('posts', PostCardList.as_view()),
+    # 회원가입 / 로그인
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+
+    path('cards/', PostCardList.as_view()),
+    path('posts/', PostCardList.as_view()),
     path('posts/<int:pk>', PostDetail.as_view()),
 ]
 
