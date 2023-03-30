21022a5cdf1f53ebc829112881de18e4446109f0
tnstjd120 <tnstjd120@gmail.com>
2023-03-30 22:56:55 +0900

    chor: 카테고리 R, 카테고리 리스트 R, 게시물 CR => URL path 생성

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import PostCardList, PostList, PostDetail
+from .views import PostCardList, PostList, PostDetail, CategoryView, CategorysView, PostCreateView
 
 urlpatterns = [
+    path('category/<int:category_id>/', CategoryView.as_view()),
+    path('categorys/<int:user_id>/', CategorysView.as_view()),
     path('cards/', PostCardList.as_view()),
     path('posts/', PostList.as_view()),
-    path('posts/<int:pk>', PostDetail.as_view()),
+    path('posts/<int:pk>/', PostDetail.as_view()),
+    path('post/create/', PostCreateView.as_view()),
+    
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
