cebe9d55cba43c59d35399a1cabd21f33bff3846
tnstjd120 <tnstjd120@gmail.com>
2023-04-05 02:43:02 +0900

    fix: User model 수정

--- accounts/models.py
+++ accounts/models.py
@@ -10,13 +10,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=30, unique=True, null=False, blank=False, help_text="이메일")
     password = models.CharField(max_length=255, help_text="비밀번호")
     profile_img = models.CharField(max_length=255, default="", help_text="프로필 이미지")
-    about = models.TextField(null=True, blank=True, db_collation='utf8mb4_unicode_ci', help_text="내 소개")
+    about = models.TextField(null=True, blank=True, default="내 정보에서 내 소개를 입력해서 변경해주세요!", db_collation='utf8mb4_unicode_ci', help_text="내 소개")
     phone = models.CharField(max_length=13, null=True, blank=True, help_text="핸드폰 번호")
     blog_id = models.CharField(max_length=20, null=True, blank=True, unique=True, help_text="블로그 아이디")
-    blog_name = models.CharField(max_length=20, default="CardLog", help_text="블로그 이름")
+    blog_name = models.CharField(max_length=20, default="카드로그 제목", help_text="블로그 이름")
     
-    github_url = models.CharField(max_length=255, default="", help_text="github url")
-    blog_url = models.CharField(max_length=255, default="", help_text="blog url")
+    github_url = models.CharField(max_length=255, null=True, blank=True, default="", help_text="github url")
+    blog_url = models.CharField(max_length=255, null=True, blank=True, default="", help_text="blog url")
 
     is_active = models.BooleanField(default=True, help_text="접근 권한 - True: 접속 가능 / False: 접속 불가")
     is_superuser = models.BooleanField(default=False)
