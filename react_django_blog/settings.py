2480413b002cd642c7c42aa4ef9bbcbfbe40b9f3
tnstjd120 <tnstjd120@gmail.com>
2023-04-05 02:56:53 +0900

    fix: ALLOWED_HOST 변경

--- react_django_blog/settings.py
+++ react_django_blog/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 # CORS 허용
 CSRF_TRUSTED_ORIGINS = [
